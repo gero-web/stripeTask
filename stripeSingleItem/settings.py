85d13e0699af9d7d0400dff75599dc4b3a804bac
gero-web <78724836+gero-web@users.noreply.github.com>
2023-02-17 13:58:36 +0800

    Update settings.py

--- stripeSingleItem/settings.py
+++ stripeSingleItem/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-9*&9c_xlfhj7m-vhx-#)mt-5yp%=w1wjdbnbxcmb7c_54(sc!8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
